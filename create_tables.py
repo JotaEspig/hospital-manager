b84ff62c1826c77f574c146416f3e628c98ff1a6
João Vitor Espig <jotinha1300@gmail.com>
2022-10-15 23:29:42 -0300

    changed import

--- create_tables.py
+++ create_tables.py
@@ -1,14 +1,14 @@
 from config.config import db
-from models import * # Gets the models to "db.create_all()" create
+import models # Gets the models to "db.create_all()" create
 
 
 if __name__ == "__main__":
     db.create_all()
 
-    if admin.Admin.query.filter_by(username="admin").first() != None:
+    if models.admin.Admin.query.filter_by(username="admin").first() != None:
         exit(0)
-    
-    a = admin.Admin(username="admin", pwhash="admin") # TODO MUDAR ISSO
+
+    a = models.admin.Admin(username="admin", pwhash="admin")
     db.session.add(a)
     db.session.commit()
     
