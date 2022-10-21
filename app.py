9622e6ee14ff1df7b2dd1af54e3cc714bcd7b065
João Vitor Espig <jotinha1300@gmail.com>
2022-10-20 22:59:11 -0300

    fixed error

--- app.py
+++ app.py
@@ -3,5 +3,7 @@ import controllers # Gets the routes
 
 
 if __name__ == "__main__":
-    db.create_all()
+    with app.app_context():
+        db.create_all()
+    
     app.run(host="0.0.0.0", debug=True)
